38fbac375479a6ac98cb02928acf3c6cac49bbb0
himanajju <himanshuinspl@gmail.com>
2019-07-20 20:48:11 -0700

    CSRF added

--- include/Functions.php
+++ include/Functions.php
@@ -4,6 +4,37 @@ function Redirect_to($New_Location){
     exit;
 }
 
+/**
+ * generate CSRF token
+ * 
+ * @param   string $formName
+ * @return  string  
+ */
+
+
+function generateToken( $formName ) 
+{
+    $secretKey = 'gsfhs154aergz2#';
+    if ( !session_id() ) {
+        session_start();
+    }
+    $sessionId = session_id();
+
+    return sha1( $formName.$sessionId.$secretKey );
+
+}
+
+/**
+ * check CSRF token
+ * 
+* @param   string $token
+ * @param   string $formName
+ * @return  boolean  
+ */
+function checkToken( $token, $formName ) 
+{
+    return $token === generateToken( $formName );
+}
 
 
 ?>
